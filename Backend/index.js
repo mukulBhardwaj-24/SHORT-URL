06d52f48da0893ae12fd3120f0b86c0cbbad814c
Mukul Sharma <mukulbhardwaj531@gmail.com>
2024-07-21 23:55:50 +0530

    route setup checking

--- Backend/index.js
+++ Backend/index.js
@@ -4,6 +4,9 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+app.get("/", (req, res) => {
+    res.send(`Home Page`);
+})
 app.listen(PORT, () => {
     console.log(`Server has started at port: ${process.env.PORT}`);
 })
