73554679c29e50ac4eadd37e6a750f298ef8eb15
Mukul Sharma <mukulbhardwaj531@gmail.com>
2024-07-23 23:39:01 +0530

    added new route

--- Backend/index.js
+++ Backend/index.js
@@ -1,11 +1,22 @@
 require('dotenv').config()
 const express = require("express");
+const URL = require("./model/url.model.js")
 
 const app = express();
 const PORT = 3000;
 
-app.get("/", (req, res) => {
-    res.send(`Home Page`);
+app.get("/:shortId", async (req, res) => {
+    const shortId = req.params.shortId;
+    await URL.findOneAndUpdate(
+        {
+            shortId
+        },
+        {
+            $push: {
+                visitHistory: { timestamp: Date.now()}
+            }
+        }
+    )
 })
 app.listen(PORT, () => {
     console.log(`Server has started at port: ${process.env.PORT}`);
